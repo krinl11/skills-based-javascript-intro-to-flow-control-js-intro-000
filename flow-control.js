e94e38304ce81a5e0cb8bd15ad189b1bafb9bbcc
Karina Evers <krin_11_bball@yahoo.com>
2019-06-05 18:24:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,8 +4,9 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if (age >=13 && age <=19) {return "You are a teenager!"}
-  else {return "You are not a teenager"}
+  if (age >=13 && age <=19) {return "You are a teenager!"
+} else {
+  return "You are not a teenager"}
 }
 
 function ageChecker(age) {
