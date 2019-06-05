f1c69c3badeec3152ea84f9908e0b95b8be8eb9a
Karina Evers <krin_11_bball@yahoo.com>
2019-06-05 19:05:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,9 +28,9 @@ return (age >=13 && age <=19) ? "You are a teenager" : "You are not a teenager"
 function switchAge(age) {
   switch (age) {
     case "age >= 13 && age <=19":
-      return "You have an age";
+      return "You are a teenager";
       break;
     default:
-      return "You are a teenager"
+      return "You have an age"
   }
 }
